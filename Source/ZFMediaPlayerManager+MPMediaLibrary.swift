bc6c247bb656ed081e9faad4a615828c42657903
qiuzhifei <qiuzhifei521@gmail.com>
2018-05-11 17:46:29 +0800

    add playlist manager

--- Source/ZFMediaPlayerManager+MPMediaLibrary.swift
+++ Source/ZFMediaPlayerManager+MPMediaLibrary.swift
@@ -12,9 +12,24 @@ import MediaPlayer
 extension ZFMediaPlayerManager {
   
   @available(iOS 9.3, *)
+  open func createPlaylistIfNeeded(with uuid: UUID,
+                                   creationMetadata: MPMediaPlaylistCreationMetadata?,
+                                   completionHandler: ((MPMediaPlaylist?, Error?) -> ())?) {
+    MPMediaLibrary.default()
+      .getPlaylist(with: uuid,
+                   creationMetadata: creationMetadata) {
+                    [weak self] (list, err) in
+                    guard let `self` = self else { return }
+                    if let handler = completionHandler {
+                      handler(list, err)
+                    }
+    }
+  }
+  
+  @available(iOS 9.3, *)
   open func getPlaylist(with uuid: UUID,
-                   creationMetadata: MPMediaPlaylistCreationMetadata?,
-                   completionHandler: ((MPMediaPlaylist?, Error?) -> ())?) {
+                        creationMetadata: MPMediaPlaylistCreationMetadata?,
+                        completionHandler: ((MPMediaPlaylist?, Error?) -> ())?) {
     MPMediaLibrary.default()
       .getPlaylist(with: uuid,
                    creationMetadata: creationMetadata) {
@@ -32,6 +47,34 @@ extension ZFMediaPlayerManager {
       if let handler = handler {
         handler(err)
       }
+    }
+  }
+  
+  @available(iOS 9.3, *)
+  open func addPlaylistItems(with uuid: UUID,
+                             creationMetadata: MPMediaPlaylistCreationMetadata?,
+                             items: [String],
+                             completionHandler: ((MPMediaPlaylist?, Error?) -> ())?) {
+    self.getPlaylist(with: uuid,
+                     creationMetadata: creationMetadata) {
+                      [weak self] (playlist, error) in
+                      guard let `self` = self else { return }
+                      if let error = error {
+                        debugPrint("get playlist error: \(error)")
+                        return
+                      }
+                      guard let playlist = playlist else {
+                        debugPrint("get playlist error: playlist is nil")
+                        return
+                      }
+                      debugPrint("items == \(playlist.items)")
+                      for item in items {
+                        self.addItem(identifier: item,
+                                     mediaPlaylist: playlist,
+                                     handler: { (err) in
+                                      debugPrint("add item error: \(err)")
+                        })
+                      }
     }
   }
   
