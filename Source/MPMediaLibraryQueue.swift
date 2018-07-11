6ed05197a5e62afcfa9e19a9a7bdd4f721f7ec67
qiuzhifei <qiuzhifei521@gmail.com>
2018-07-11 16:11:11 +0800

    update sync library

--- Source/MPMediaLibraryQueue.swift
+++ Source/MPMediaLibraryQueue.swift
@@ -10,53 +10,67 @@ import MediaPlayer
 
 class MPMediaLibraryQueue: NSObject {
   
-  fileprivate var isLoading = false
+  fileprivate let queue = DispatchQueue(label: "com.apple.music.sync")
   fileprivate var configs: [MPMediaPlaylist: MPMediaLibraryQueueConfig] = [:]
   
   @available(iOS 9.3, *)
   open func addItems(_ items: [String], mediaPlaylist: MPMediaPlaylist) {
     let config = self.getConfig(mediaPlaylist: mediaPlaylist)
+    if config.items.count != 0 {
+      config.add(items: items)
+      return
+    }
+    
     config.add(items: items)
-    self.addConfig(config)
+    self.queue.async {
+      self.startSyncTask(config: config)
+    }
   }
   
   @available(iOS 9.3, *)
   open func addItem(_ item: String, mediaPlaylist: MPMediaPlaylist) {
-    let config = self.getConfig(mediaPlaylist: mediaPlaylist)
-    config.add(item: item)
-    self.addConfig(config)
+    self.addItems([item], mediaPlaylist: mediaPlaylist)
   }
   
 }
 
 fileprivate extension MPMediaLibraryQueue {
   
-  func addConfig(_ config: MPMediaLibraryQueueConfig) {
-    if self.isLoading {
+  func startSyncTask(config: MPMediaLibraryQueueConfig) {
+    guard let item = config.items.first else {
       return
     }
-    addIntrinsicConfig(config)
-  }
-  
-  func addIntrinsicConfig(_ config: MPMediaLibraryQueueConfig) {
-    if config.items.count == 0 {
-      self.isLoading = false
-      return
-    }
-    self.isLoading = true
-    let item = config.items.first!
+    
     if #available(iOS 9.3, *) {
       config.mediaPlaylist.addItem(withProductID: item) {
         [weak self] (err) in
         guard let `self` = self else { return }
-        DispatchQueue.main.async {
-          config.remove(item: item)
-          self.addIntrinsicConfig(config)
+        if let err = err as? MPError {
+          debugPrint("err == ", err.code.rawValue)
+          if err.code == MPError.Code.permissionDenied || err.code == MPError.Code.cloudServiceCapabilityMissing {
+            // 不支持 sync，停止 sync
+            self.endSyncTask(config: config)
+            return
+          }
         }
+        
+        self.syncTaskCompleted(config: config, item: item, err: err)
       }
     } else {
       // Fallback on earlier versions
     }
+  }
+  
+  func syncTaskCompleted(config: MPMediaLibraryQueueConfig,  item: String, err: Error?) {
+    config.remove(item: item)
+    
+    self.queue.async {
+      self.startSyncTask(config: config)
+    }
+  }
+  
+  func endSyncTask(config: MPMediaLibraryQueueConfig) {
+    config.items = []
   }
   
   func getConfig(mediaPlaylist: MPMediaPlaylist) -> MPMediaLibraryQueueConfig {
